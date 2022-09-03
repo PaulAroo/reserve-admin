6e8dac6e04bb126a1791060c65654ab87c8fe9eb
Paul_Aroo <timmypaul2006@gmail.com>
2022-09-03 14:16:16 +0100

    implement: login and auth context

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import { AuthContextProvider } from "./context/auth-context";
 import { DarkModeContextProvider } from "./context/darkModeContext";
 import "./index.scss";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <DarkModeContextProvider>
-      <App />
-    </DarkModeContextProvider>
+    <AuthContextProvider>
+      <DarkModeContextProvider>
+        <App />
+      </DarkModeContextProvider>
+    </AuthContextProvider>
   </React.StrictMode>
 );
--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,7 +1,67 @@
+import { useContext, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { AuthContext } from "../../context/auth-context";
+
 import "./login.scss";
+import axios from "../../axios";
+import VisibilityIcon from "@mui/icons-material/Visibility";
+import VisibilityOffIcon from "@mui/icons-material/VisibilityOff";
 
 function Login() {
-  return <div>Login</div>;
+  const navigate = useNavigate();
+  const [userDetails, setUserDetails] = useState({
+    username: undefined,
+    password: undefined,
+  });
+  const [revealPassword, setRevealPassword] = useState(false);
+  const { dispatch, loading, error } = useContext(AuthContext);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    // dispatch({ type: "LOGIN_START" });
+    // try {
+    //   const res = await axios.post("/auth/login", userDetails);
+    //   dispatch({ type: "LOGIN_SUCCESS", payload: res.data.details });
+    //   navigate("/");
+    // } catch (err) {
+    //   console.log(err);
+    //   dispatch({ type: "LOGIN_FAILURE", payload: err.response.data });
+    // }
+  };
+
+  const handleChange = (e) => {
+    setUserDetails((prev) => ({ ...prev, [e.target.id]: e.target.value }));
+  };
+
+  const togglePassordInputType = () => {
+    setRevealPassword((prev) => !prev);
+  };
+
+  return (
+    <div className="login">
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="username">
+          UserName
+          <input type="text" id="username" onChange={handleChange} />
+        </label>
+        <label htmlFor="password">
+          Password
+          <div className="pass">
+            <input
+              type={revealPassword ? "text" : "password"}
+              id="password"
+              onChange={handleChange}
+            />
+            <span onClick={togglePassordInputType}>
+              {revealPassword ? <VisibilityIcon /> : <VisibilityOffIcon />}
+            </span>
+          </div>
+        </label>
+        <button disabled={loading}>Login</button>
+        {error && <p>{error.message}</p>}
+      </form>
+    </div>
+  );
 }
 
 export default Login;
