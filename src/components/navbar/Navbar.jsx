939f95d910b6ed1d5be77b0d99b1aa6929a57264
Paul_Aroo <timmypaul2006@gmail.com>
2023-02-16 21:56:33 +0100

    wip: full screen

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,20 +1,32 @@
 import "./navbar.scss";
+import LightModeIcon from "@mui/icons-material/LightMode";
+import FullscreenIcon from "@mui/icons-material/Fullscreen";
+import ListOutlinedIcon from "@mui/icons-material/ListOutlined";
 import SearchOutlinedIcon from "@mui/icons-material/SearchOutlined";
-import LanguageOutlinedIcon from "@mui/icons-material/LanguageOutlined";
 import DarkModeOutlinedIcon from "@mui/icons-material/DarkModeOutlined";
+import LanguageOutlinedIcon from "@mui/icons-material/LanguageOutlined";
 import FullscreenExitOutlinedIcon from "@mui/icons-material/FullscreenExitOutlined";
 import NotificationsNoneOutlinedIcon from "@mui/icons-material/NotificationsNoneOutlined";
 import ChatBubbleOutlineOutlinedIcon from "@mui/icons-material/ChatBubbleOutlineOutlined";
-import ListOutlinedIcon from "@mui/icons-material/ListOutlined";
-import LightModeIcon from "@mui/icons-material/LightMode";
 
 import { useContext, useState } from "react";
 import { DarkModeContext } from "../../context/darkModeContext";
+import { useEffect } from "react";
 
 function Navbar() {
   const [searchInputFocus, setSearchInputFocus] = useState(false);
+  const [isFullscreenEnabled, setIsFullScreenEnabled] = useState(false);
 
   const { darkMode, dispatch } = useContext(DarkModeContext);
+
+  const requestFullScreen = () => {
+    const isInFullscreen = !!document.fullscreenElement;
+    isInFullscreen
+      ? document.exitFullscreen()
+      : document.body.requestFullscreen();
+
+    setIsFullScreenEnabled((prev) => !prev);
+  };
 
   return (
     <div className="navbar">
@@ -40,8 +52,12 @@ function Navbar() {
               <DarkModeOutlinedIcon className="icon" />
             )}
           </div>
-          <div className="item">
-            <FullscreenExitOutlinedIcon className="icon" />
+          <div onClick={requestFullScreen} className="item">
+            {isFullscreenEnabled ? (
+              <FullscreenExitOutlinedIcon className="icon" />
+            ) : (
+              <FullscreenIcon className="icon" />
+            )}
           </div>
           <div className="item">
             <NotificationsNoneOutlinedIcon className="icon" />
@@ -56,7 +72,7 @@ function Navbar() {
           </div>
           <div className="item">
             <img
-              src="https://images.pexels.com/photos/733872/pexels-photo-733872.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260"
+              src="https://i.ibb.co/xGWh0Ry/profile.jpg"
               alt=""
               className="avatar"
             />
