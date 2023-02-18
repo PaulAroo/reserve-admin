2fc849b1e9b29d3b44a8713227dd6b1479d69b97
Paul_Aroo <timmypaul2006@gmail.com>
2023-02-18 05:51:05 +0100

    setup: release workflow

--- src/App.jsx
+++ src/App.jsx
@@ -32,7 +32,7 @@ function App() {
   };
   return (
     <div className={`App ${darkMode ? "dark" : ""}`}>
-      <Router>
+      <Router basename="/reserve-admin">
         <Routes>
           <Route path="/">
             <Route
